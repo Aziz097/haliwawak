1225113973fddfa024fdd08d70946662e15a65d6
Aziz Kurniawan <aziz.122140097@student.itera.ac.id>
2026-06-16 00:27:19 +0700

    data: add corrected 25-species seed from DOCX

---
+++ src/db/seed-species-corrected.ts
@@ -0,0 +1,31 @@
+export const CORRECTED_SPECIES = [
+  // Papilionidae (6)
+  { commonName: 'Kupu-kupu Ekor Hijau', scientificName: 'Graphium agamemnon', family: 'Papilionidae' },
+  { commonName: 'Kupu-kupu Ekor Biru', scientificName: 'Graphium doson', family: 'Papilionidae' },
+  { commonName: 'Kupu-kupu Sawah Hijau', scientificName: 'Graphium sarpedon', family: 'Papilionidae' },
+  { commonName: 'Kupu-kupu Lemon', scientificName: 'Papilio demoleus', family: 'Papilionidae' },
+  { commonName: 'Kupu-kupu Memnon', scientificName: 'Papilio memnon', family: 'Papilionidae' },
+  { commonName: 'Kupu-kupu Ekor Walet Jeruk', scientificName: 'Papilio polytes', family: 'Papilionidae' },
+  // Pieridae (6)
+  { commonName: 'Kupu-kupu Putih Olferna Jantan', scientificName: 'Appias olferna (jantan)', family: 'Pieridae' },
+  { commonName: 'Kupu-kupu Putih Olferna Betina', scientificName: 'Appias olferna (betina)', family: 'Pieridae' },
+  { commonName: 'Kupu-kupu Merah Delias', scientificName: 'Delias hyparete', family: 'Pieridae' },
+  { commonName: 'Kupu-kupu Kuning Blanda', scientificName: 'Eurema blanda', family: 'Pieridae' },
+  { commonName: 'Kupu-kupu Kuning Piranthe', scientificName: 'Catopsilia pyranthe', family: 'Pieridae' },
+  { commonName: 'Kupu-kupu Kertas', scientificName: 'Leptosia nina', family: 'Pieridae' },
+  // Nymphalidae (12)
+  { commonName: 'Kupu-kupu Terpsicore', scientificName: 'Acraea terpsicore', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Biru Orithya Jantan', scientificName: 'Junonia orithya (jantan)', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Biru Orithya Betina', scientificName: 'Junonia orithya (betina)', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Garis Putih', scientificName: 'Neptis hylas', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Missipus Jantan', scientificName: 'Hypolimnas missipus (jantan)', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Missipus Betina', scientificName: 'Hypolimnas missipus (betina)', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Daun Kering', scientificName: 'Doleschallia bisaltide', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Hitam Putih', scientificName: 'Euploea leucostictos', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Bolina Jantan', scientificName: 'Hypolimnas bolina (jantan)', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Bolina Betina', scientificName: 'Hypolimnas bolina (betina)', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Mata Besar', scientificName: 'Melanitis leda', family: 'Nymphalidae' },
+  { commonName: 'Kupu-kupu Coklat Iphita', scientificName: 'Junonia iphita', family: 'Nymphalidae' },
+  // Lycaenidae (1)
+  { commonName: 'Kupu-kupu Zizina', scientificName: 'Zizina otis', family: 'Lycaenidae' },
+] as const;
